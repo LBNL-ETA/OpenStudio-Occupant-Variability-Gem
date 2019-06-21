31fd4725aa22d1b8281bf769702abd169a1f611a
Han Li <hanlibpd@gmail.com>
2019-06-21 11:23:57 -0700

    Added the baseline.osw and start configuring the OSWs

--- lib/openstudio/occupant_variability/apply_occupancy_simulator.rb
+++ lib/openstudio/occupant_variability/apply_occupancy_simulator.rb
@@ -1,9 +1,31 @@
 module OpenStudio
   module OccupantVariability
-    class ApplyOccupancySimulator
+    class OccupancySimulatorApplier
 
       # class level variables
+      @@instance_lock = Mutex.new
       @@osw = nil
+
+      def initialize(baseline_osw_dir)
+        # do initialization of class variables in thread safe way
+        @@instance_lock.synchronize do
+          if @@osw.nil?
+
+            # load the OSW for this class
+            osw_path = File.join(baseline_osw_dir, 'baseline.osw')
+            File.open(osw_path, 'r') do |file|
+              @@osw = JSON.parse(file.read, symbolize_names: true)
+            end
+
+            # add any paths local to the project
+            @@osw[:file_paths] << File.join(File.dirname(__FILE__), '../weather/')
+
+            # configures OSW with extension gem paths for measures and files, all extension gems must be
+            # required before this
+            @@osw = OpenStudio::Extension.configure_osw(@@osw)
+          end
+        end
+      end
 
       def create_osw(userLib_csv_dir)
 
@@ -11,8 +33,14 @@ module OpenStudio
 
         puts 'Here we are.'
 
-        # OpenStudio::Extension.set_measure_argument(osw, 'ReduceElectricEquipmentLoadsByPercentage', '__SKIP__', false)
+        # puts OpenStudio
+        puts OpenStudio::Extension.methods
+
+        # OpenStudio::Extension.set_measure_argument(osw, 'Occupancy_Simulator', '__SKIP__', false)
         # OpenStudio::Extension.set_measure_argument(osw, 'ReduceLightingLoadsByPercentage', '__SKIP__', false)
+
+
+        puts osw
 
         return osw
       end
--- lib/openstudio/occupant_variability/extension.rb
+++ lib/openstudio/occupant_variability/extension.rb
@@ -17,7 +17,7 @@ module OpenStudio
       # Relevant files such as weather data, design days, etc.
       # Return the absolute path of the files or nil if there is none, used when configuring OSWs
       def files_dir
-        return nil
+        return File.absolute_path(File.join(@root_dir, 'lib', 'files'))
       end
 
       # Doc templates are common files like copyright files which are used to update measures and other code
